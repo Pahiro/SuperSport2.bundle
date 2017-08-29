c47c45e19af5fa9ceaad4e319ee23c66a0c601b0
Pahiro <bennetvdg@gmail.com>
2017-08-29 14:39:57 +0200

    Swap Cookies for Sessions
    Test session code. Not working either. Wonder if original Supersport.bundle handles this request in a way I haven't figured out. I'll check again tomorrow.

--- Contents/Code/test.py
+++ Contents/Code/test.py
@@ -5,26 +5,28 @@ import httplib
 import requests 
 import auth
 
-LIVE_STREAMS_URL = "https://www.supersport.com/AjaxOperation.aspx/GetVideoStreams"
+session = requests.Session()
 
-auth.login()
-cookie = auth.check_auth()
+response = session.post(
+    'https://connect.dstv.com/4.1/en-ZA/Login',
+    data={'email':'email', 'password':'password'}
+)
 
-if cookie == False:
-    cookie = auth.login()
-for item in cookie:    
-    headers = { 'Host' : 'www.supersport.com',
-                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
-                'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language' : 'en-US,en;q=0.5',
-                'Accept-Encoding' : 'gzip, deflate, br',
-                'Cookie' : item.name + '=' + item.value, 
-                'Connection' : 'keep-alive',
-                'Upgrade-Insecure-Requests': '1',
-                'Cache-Control' : 'max-age=0' }
+print response.status_code #200 - Logs in Successfully
+
+headers = { 'Host' : 'www.supersport.com',
+            'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language' : 'en-US,en;q=0.5',
+            'Accept-Encoding' : 'gzip, deflate, br', 
+            'Connection' : 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Cache-Control' : 'max-age=0' }
 
 
-r = requests.get("https://www.supersport.com/video/playerlivejson.aspx?vid=104264", headers=headers, cookies=cookie)
+#Still get an auth failure.
+r = session.get("https://www.supersport.com/video/playerlivejson.aspx?vid=104264", headers=headers)
+#r = requests.get("https://www.supersport.com/video/playerlivejson.aspx?vid=104264", headers=headers)
 
 print(r.text + "\n") #Working
 
